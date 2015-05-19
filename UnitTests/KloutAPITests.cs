6d29a775177f82a4c748e71cfe6191f3bb9d8644
Josh Keegan <josh@visav.co.uk>
2015-05-19 16:36:44 +0100

    Unit Tests

---
+++ UnitTests/KloutAPITests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using MonoKlout;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class KloutAPITests
+    {
+        //Constants
+        private const string KLOUT_API_KEY_FILE_PATH = "../../KLOUT_API_KEY";
+
+        //Variables
+        private static string kloutApiKey;
+
+        [SetUp]
+        public void SetUp()
+        {
+            kloutApiKey = File.ReadAllText(KLOUT_API_KEY_FILE_PATH).Trim();
+        }
+
+        [Test]
+        public void TestGetKloutIdentity()
+        {
+            KloutAPI kloutApi = new KloutAPI(kloutApiKey, "kevin");
+            const string expected = "42502726249709279";
+
+            KloutIdentityResponse response = kloutApi.GetKloutIdentity();
+
+            Assert.AreEqual(expected, response.id);
+        }
+
+        [Test]
+        public void TestGetKloutIdentityDoesntExist()
+        {
+            KloutAPI kloutApi = new KloutAPI(kloutApiKey, "kshkahashfkljah123");
+
+            KloutIdentityResponse response = kloutApi.GetKloutIdentity();
+
+            Assert.IsNull(response);
+        }
+    }
+}
